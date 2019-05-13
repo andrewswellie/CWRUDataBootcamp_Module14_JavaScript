bbb345d5b8e7c4c2c3e342b4bea1d2bcaebe3447
Andrew Swellie <andrewswellie@gmail.com>
2019-05-13 14:33:37 -0400

    early save of my .js file

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Use d3 and forEach loops to pull data from the .js file and load it into our HTML dynamically
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(buildData) {
+    var row = tbody.append("tr");
+  
+    Object.entries(buildData).forEach(function([key, value]) {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+  
+
