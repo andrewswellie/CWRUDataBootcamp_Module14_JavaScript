ad70edae5f46b5eff7706f3f839987ed3b743ef6
Andrew Swellie <andrewswellie@gmail.com>
2019-05-13 16:07:47 -0400

    another save point for my .js file

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,17 +1,33 @@
 // from data.js
 var tableData = data;
 
-// Use d3 and forEach loops to pull data from the .js file and load it into our HTML dynamically
+// Use d3 to select our body elements and button ID and store them in variables.
 var tbody = d3.select("tbody");
+var filterBtn = d3.select("#filter-btn");
 
+// Use d3 and forEach loops to pull data from the .js file and load it into our HTML dynamically
 tableData.forEach(function(buildData) {
     var row = tbody.append("tr");
-  
     Object.entries(buildData).forEach(function([key, value]) {
       var cell = row.append("td");
       cell.text(value);
     });
   });
 
-  
 
+// Create an event handler for the filter button click that generates a new list of entries and loads them based on the date input
+filterBtn.on("click", function() {
+  d3.event.preventDefault();
+  var inputField = d3.select("#datetime");
+  var queryDate = inputField.property('value');
+  var filteredData = tableData.filter(function (sighting) {
+      	return sighting.datetime === queryDate;
+      });
+  tbody.text('');
+  filteredData.forEach(record => { 
+          	var row = tbody.append('tr');
+            Object.entries(record).forEach( ([key, value]) => {
+            row.append('td').text(value);
+        });
+    });
+});
